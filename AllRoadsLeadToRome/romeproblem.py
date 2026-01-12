3aa97e8536b8222d3e9948869ab3a47b9fc4b70c
LiveMaas <122502054+LiveMaas@users.noreply.github.com>
2026-01-12 17:15:45 -0600

    Adding question base code for testing

--- AllRoadsLeadToRome/romeproblem.py
+++ AllRoadsLeadToRome/romeproblem.py
@@ -6,3 +6,16 @@
 # {} connections lead to Rome with an average flight time of {} minutes
 # Replace {} with the number of connections and the average flight time.
 
+connections = [
+    ('Amsterdam', 'Dublin', 100),
+    ('Amsterdam', 'Rome', 140),
+    ('Rome', 'Warsaw', 130),
+    ('Minsk', 'Prague', 95),
+    ('Stockholm', 'Rome', 190),
+    ('Copenhagen', 'Paris', 120),
+    ('Madrid', 'Rome', 135),
+    ('Lisbon', 'Rome', 170),
+    ('Dublin', 'Rome', 170),
+    ]
+    
+for city in connections
